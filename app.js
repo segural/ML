2d5627ebacd1011216b5ac191d2e54fd3009b7eb
segural <luciano.g.segura@gmail.com>
2021-01-15 18:45:21 -0300

    inserto cmentarios para mayor organizacion

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//requiero paquetes y genero las variables(espress,path)
 const express = require ('express');
 const path = require ('path');
 const app = express ();
@@ -5,10 +6,12 @@ const publicPath = path.resolve ('public');
 
 app.use(express.static (publicPath));
 
+//hago la peticion al server de las htmls
 app.get ('/', (req,res) =>{
     res.sendFile (path.resolve ('views/home.html'))
 })
 
+//levanto server express en puerto 3000
 app.listen (3000, () => {
     console.log ('Servidor corriendo en puerto 3000');
 });
